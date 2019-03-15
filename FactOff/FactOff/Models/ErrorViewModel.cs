c3706b6aa1c26091683d5c25bb578c214a933ff3
Squizi <nevencheto01@gmail.com>
2019-03-15 17:36:50 +0200

    Create new ASP.NET Core project.

---
+++ FactOff/FactOff/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FactOff.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
