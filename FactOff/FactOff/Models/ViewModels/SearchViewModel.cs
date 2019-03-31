ad96d12e9f6e03db254ee0484faa76be24c6c51f
Kalin <doiychevk@gmail.com>
2019-03-31 15:46:47 +0300

    Updated gitignore

--- FactOff/FactOff/Models/ViewModels/SearchViewModel.cs
+++ FactOff/FactOff/Models/ViewModels/SearchViewModel.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace FactOff.Models.ViewModels
+﻿namespace FactOff.Models.ViewModels
 {
     public class SearchViewModel
     {
-        private string creatorName;
-        private string factContent;
-        private float factRating;
-
-        public string FactContent { get => factContent; set => factContent = value; }
-        public string CreatorName { get => creatorName; set => creatorName = value; }
-        public float FactRating { get => factRating; set => factRating = value; }
+        public string FactContent { get; set; }
+        public string CreatorName { get; set; }
+        public float FactRating { get; set; }
     }
 }
