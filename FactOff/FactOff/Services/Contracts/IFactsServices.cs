fbb4d32006d7103106b532fc7d387d3e5195ff27
Kalin <doiychevk@gmail.com>
2019-03-31 18:59:06 +0300

    Started work on services

---
+++ FactOff/FactOff/Services/Contracts/IFactsServices.cs
@@ -0,0 +1,11 @@
+﻿using FactOff.Models.DB2;
+using System;
+using System.Collections.Generic;
+
+namespace FactOff.Services.Contracts
+{
+    interface IFactsService
+    {
+        Guid CreateFact(string context, ICollection<FactsTags> tags);
+    }
+}
---
+++ FactOff/FactOff/Services/FactsServices.cs
@@ -0,0 +1,28 @@
+﻿using FactOff.Models.DB2;
+using FactOff.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FactOff.Services
+{
+    public class FactsService : IFactsService
+    {
+        private FactOffContext context;
+        public FactsService(FactOffContext context)
+        {
+            this.context = context;
+        }
+        public Guid CreateFact(string factContext, ICollection<FactsTags> tags)
+        {
+            Fact fact = new Fact()
+            {
+                Context = factContext,
+                Tags = tags
+            };
+
+            return fact.FactId;
+        }
+    }
+}
