f7e3899ea752020a31a51842d3486f2322179edd
Kalin <doiychevk@gmail.com>
2019-04-06 16:21:04 +0300

    XML controllers documentation done

--- FactOff/FactOff/Controllers/HomeController.cs
+++ FactOff/FactOff/Controllers/HomeController.cs
@@ -7,27 +7,47 @@ using System.Linq;
 
 namespace FactOff.Controllers
 {
+    /// <summary>
+    /// Responsible for all views in the Home folder.
+    /// </summary>
     public class HomeController : Controller
     {
-        private readonly IThemesService serviceTheme;
-        private readonly IFactsService serviceFacts;
+        /// <summary>
+        /// Allows communication with the <c>ThemesService class</c>.
+        /// </summary>
+        private readonly IThemesService themesService;
+        /// <summary>
+        /// Allows communication with the <c>FactsService class</c>.
+        /// </summary>
+        private readonly IFactsService factsService;
 
-        public HomeController(IThemesService serviceTheme, IFactsService serviceFacts)
+        /// <summary>
+        /// Initializes a new instance of the HomeController class.
+        /// It's being called by the StartUp class.
+        /// </summary>
+        /// <param name="themesService">A required service for the class.</param>
+        /// <param name="factsService">A required service for the class.</param>
+        public HomeController(IThemesService themesService, IFactsService factsService)
         {
-            this.serviceTheme = serviceTheme;
-            this.serviceFacts = serviceFacts;
+            this.themesService = themesService;
+            this.factsService = factsService;
         }
 
+        /// <summary>
+        /// The main page. Displays ten rendom facts and some themes.
+        /// Redirects to the Index page in the Home folder.
+        /// </summary>
+        /// <returns>Rendered view to the response.</returns>
         public IActionResult Index()
         {
             var model = new HomeViewModel
             {
-                Themes = serviceTheme.GetAllThemes().Select(t => new HomeThemeViewModel
+                Themes = themesService.GetAllThemes().Select(t => new HomeThemeViewModel
                 {
                     ThemeId = t.ThemeId,
                     Name = t.Name
                 }),
-                Facts = serviceFacts.GetRandomTen().Select(f => new HomeFactViewModel
+                Facts = factsService.GetRandomTen().Select(f => new HomeFactViewModel
                 {
                     FactId = f.FactId,
                     Context = f.Context,
@@ -43,13 +63,11 @@ namespace FactOff.Controllers
             return View(model);
         }
 
-        public IActionResult Facts()
-        {
-            ViewData["Message"] = "Your application page full of facts.";
-
-            return View();
-        }
-
+        /// <summary>
+        /// Lets the user contact the developers if he encounters a problem.
+        /// Redirect to the Contact page in the Home folder.
+        /// </summary>
+        /// <returns>Rendered view to the response.</returns>
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
@@ -57,16 +75,32 @@ namespace FactOff.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Shows the user the privacy policy.
+        /// Redirects to the Privacy page in the Home folder.
+        /// </summary>
+        /// <returns>Rendered view to the response.</returns>
         public IActionResult Privacy()
         {
             return View();
         }
 
+        /// <summary>
+        /// If the user tries to reach a page that doesn't exist he is redirected to 404 Page Not Found.
+        /// Redirects to the PageNotFound file in the Shared folder.
+        /// </summary>
+        /// <returns>Rendered view to the response.</returns>
         public IActionResult PageNotFound()
         {
-            return View();
+            return RedirectToAction("PageNotFount", "Shared");
         }
 
+
+        /// <summary>
+        /// In case of an error tells the user.
+        /// Redirects to the Error page in the Shared folder.
+        /// </summary>
+        /// <returns>Rendered view to the response.</returns>
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- FactOff/FactOff/Controllers/ThemesController.cs
+++ FactOff/FactOff/Controllers/ThemesController.cs
@@ -7,27 +7,53 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FactOff.Controllers
 {
+    /// <summary>
+    /// Only logged in users can access it.
+    /// Responsible for all views in the Themes folder.
+    /// </summary>
     public class ThemesController : Controller
     {
+        /// <summary>
+        /// Allows communication wuth the ThemesService class.
+        /// </summary>
         private readonly IThemesService service;
 
+        /// <summary>
+        /// Initializes a new instance of the ThemesController class.
+        /// It's being called by the StartUp class.
+        /// </summary>
+        /// <param name="service">A required service for the class.</param>
         public ThemesController(IThemesService service)
         {
             this.service = service;
         }
 
+        /// <summary>
+        /// Redirects to the Index page in the Themes folder.
+        /// </summary>
+        /// <returns>Rendered view to the response.</returns>
         [FactOffAuthorize]
         public IActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// Opens a form for the creation of a new theme.
+        /// Redirects to the Create page in the Themes folder.
+        /// </summary>
+        /// <returns>Rendered view to the response.</returns>
         [FactOffAuthorize]
         public IActionResult Create()
         {
             return View();
         }
-
+        
+        /// <summary>
+        /// Creates a new theme.
+        /// </summary>
+        /// <param name="request">The model for the new theme.</param>
+        /// <returns></returns>
         [FactOffAuthorize]
         [HttpPost]
         public IActionResult Create(ThemeViewModel request)
@@ -46,6 +72,11 @@ namespace FactOff.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Return the image of the theme given the <paramref name="themeId"/>.
+        /// </summary>
+        /// <param name="themeId">The id of the theme.</param>
+        /// <returns>The image of the theme.</returns>
         public FileStreamResult GetThemeImage(Guid themeId)
         {
             var theme = service.GetThemeById(themeId);
